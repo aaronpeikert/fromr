7f32c74ccda81df1d197a6669e651cffdda164c4
Aaron Peikert <aaron.peikert@me.com>
2017-06-15 13:36:43 +0200

    some beauty corrections in get_results

--- R/get_results.R
+++ R/get_results.R
@@ -2,6 +2,8 @@
 #'
 #' \code{get_results} returns the data of one or more surveys from one formr run
 #'
+#' Make shure you initialized a formr session via \code{\link{formr_api_access_token}} bevor you use \code{get_results}.
+#'
 #' @param run specify wich formr run should be used (must be one)
 #' @param survey specify wich surveys from the specified run should be downloaded (can be more the one in one character vector)
 #' @param items currently not used (don't change to something different then null)
@@ -9,8 +11,8 @@
 #' @seealso \code{\link{prettify}},\code{\link{formr_api_access_token}}
 #' @export
 get_results <- function(run,surveys,items=NULL,do_prettify=TRUE){
-  message("This may take some time. If you load many or long surveys with many participants, go grab a coffee.")
   if(is.null(formr::formr_api_session()))stop("\nNo formr session initialized.",'\nUse formr_api_access_token(< MyToken1 >,< MyToken2 >).')
+  message("This may take some time. If you load many or long surveys with many participants, go grab a coffee.")
   RUN <- run
   SURVEYS <- surveys
   ITEMS <- items
